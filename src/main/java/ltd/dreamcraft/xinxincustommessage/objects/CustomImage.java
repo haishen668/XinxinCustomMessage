424ca28ec64153387265b0b6ed43a9d5fbecf1b5
haishen668 <2821396723@qq.com>
2024-06-14 18:25:47 +0800

    1.0.6 添加了图片和文字能够水平居中绘制

--- src/main/java/ltd/dreamcraft/xinxincustommessage/objects/CustomImage.java
+++ src/main/java/ltd/dreamcraft/xinxincustommessage/objects/CustomImage.java
@@ -1,6 +1,5 @@
 package ltd.dreamcraft.xinxincustommessage.objects;
 
-
 import com.xinxin.BotApi.BotBind;
 import ltd.dreamcraft.www.pokemonbag.Utils.ParsePokemon;
 import ltd.dreamcraft.xinxincustommessage.XinxinCustomMessage;
@@ -20,16 +19,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomImage {
+    // 图片的唯一标识符
     public final String id;
 
+    // 图片的来源路径
     public final String source;
 
+    // 图片的宽度
     public final int width;
 
+    // 图片的高度
     public final int height;
 
+    // 包含的子图片列表
     public final List<SubImage> subImages;
 
+    // 包含的自定义文本列表
     public final List<CustomText> customTexts;
 
     public CustomImage(String id, String source, int width, int height, List<SubImage> subImages, List<CustomText> customTexts) {
@@ -41,6 +46,7 @@ public class CustomImage {
         this.customTexts = customTexts;
     }
 
+    // 下载图片
     public static BufferedImage downloadImage(String link) {
         try {
             URL url = new URL(link);
@@ -56,12 +62,14 @@ public class CustomImage {
             in.close();
             return image;
         } catch (Exception e) {
-            if (XinxinCustomMessage.getInstance().getConfig().getBoolean("debug"))
+            if (XinxinCustomMessage.getInstance().getConfig().getBoolean("debug")) {
                 e.printStackTrace();
+            }
             return null;
         }
     }
 
+    // 调整图片大小
     public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
         Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, 16);
         BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, 1);
@@ -72,10 +80,11 @@ public class CustomImage {
         return outputImage;
     }
 
+    // 渲染图片
     public BufferedImage renderImage(OfflinePlayer player) throws Exception {
         BufferedImage srcImg;
-        //----------------1.0.4bug修复--------------------
-        //this.source是底图的位置
+
+        // 解析图片路径，包括处理PlaceholderAPI和BotBind
         String path;
         if (player == null) {
             path = this.source;
@@ -85,33 +94,40 @@ public class CustomImage {
         if (player != null) {
             path = path.replace("{qq}", BotBind.getBindQQ(player.getName()).toLowerCase());
         }
-        //----------------1.0.4bug修复--------------------
+
+        // 根据路径类型加载图片
         if (path.startsWith("[url]")) {
-            //如果以[url]开头就下载这个链接地址的图片
+            // 如果以[url]开头，下载链接地址的图片
             srcImg = downloadImage(path.substring(5));
         } else {
+            // 否则读取本地图片
             File imageFolder = new File(XinxinCustomMessage.getInstance().getDataFolder(), "images");
             File imageFile = new File(imageFolder, path);
             srcImg = ImageIO.read(imageFile);
         }
-        //如果给定了宽高就重新设置宽高的大小
-        if (Math.min(this.width, this.height) > 0)
+
+        // 调整图片大小 如果给定了宽高就重新设置宽高的大小
+        if (Math.min(this.width, this.height) > 0) {
             srcImg = resizeImage(srcImg, this.width, this.height);
+        }
+
         Graphics2D g2d = srcImg.createGraphics();
-        //渲染图片底图
+        // 渲染图片底图
         g2d.drawImage(srcImg, 0, 0, srcImg.getWidth(), srcImg.getHeight(), null);
+
         for (SubImage subImage : this.subImages) {
             BufferedImage image = null;
             String subImgPath = subImage.path;
             subImgPath = PlaceholderAPI.setPlaceholders(player, subImgPath);
-            //如果子图片的path以[url]开头，那么就读取网链，如果不是就读取本地的图片
+
+            // 处理子图片路径
             if (subImgPath.startsWith("[url]")) {
                 if (player != null) {
                     subImgPath = subImgPath.replace("{qq}", BotBind.getBindQQ(player.getName()).toLowerCase());
                 }
                 image = downloadImage(subImgPath.substring(5));
             } else if (subImgPath.startsWith("[pokemon]")) {
-                //在pokemonbag插件中写一个方法，返回一个image对象
+                // 在pokemonbag插件中写一个方法，返回一个image对象
                 String imageFolder = "plugins/XinxinCustomMessage/images/pokemonImg";
                 if (player != null) {
                     image = ParsePokemon.pokemonToImg(imageFolder, subImgPath, player);
@@ -124,16 +140,26 @@ public class CustomImage {
                 File imageFile = new File(imageFolder, subImgPath);
                 image = ImageIO.read(imageFile);
             }
+
             int w = subImage.width;
             int h = subImage.height;
             int x = subImage.x;
             int z = subImage.z;
+            // 如果子图片不为空，调整大小并绘制到主图片上
             if (image != null) {
-                if (Math.min(w, h) > 0)
+                if (Math.min(w, h) > 0) {
                     image = resizeImage(image, w, h);
-                g2d.drawImage(image, x, z, null);
+                }
+                // 中心绘制
+                if (subImage.center) {
+//                    x = (srcImg.getWidth() - image.getWidth()) / 2;
+                    x -= image.getWidth() / 2;
+                    z -= image.getHeight() / 2;
+                }
+                g2d.drawImage(image, x, z, null);  // 这里的null表示绘制参数为默认值，即无特殊处理
             }
         }
+
         for (CustomText customText : this.customTexts) {
             String text = customText.text;
             try {
@@ -141,14 +167,16 @@ public class CustomImage {
                 if (player != null) {
                     text = text.replace("{qq}", BotBind.getBindQQ(player.getName()));
                 }
-//                text = MathUtil.calculateByText(text); 产生了一个bug
             } catch (IllegalStateException e) {
+                // 处理异常，打印或记录日志
                 if (XinxinCustomMessage.getInstance().getConfig().getBoolean("debug")) {
                     e.printStackTrace();
                 } else {
                     XinxinCustomMessage.getInstance().getLogger().warning("字符串: " + text + "中的变量无法正常解析,可能变量无法异步获取,请打开debug查看详细信息");
                 }
             }
+
+            // 处理包含Pokemon的文本
             if (text.contains("[pokemon]")) {
                 if (player != null) {
                     text = ParsePokemon.pokemonToString(text, player);
@@ -156,47 +184,54 @@ public class CustomImage {
                 if (text == null) {
                     continue;
                 }
-                //测试语句
-//                System.out.println("pokemon"+text);
             }
+
             int x = customText.x;
             int z = customText.z;
             Font font = customText.font;
-            //使用字符串切割工具切割
-            ArrayList<String> SubString = TextSplitUtil.TextSpit(text);
-            //被解析过的字符串【删除了颜色符号】
-            ArrayList<String> ParseSubText = new ArrayList<>();
-            if (SubString.isEmpty()) {
-                //颜色提取
-                Color color = TextSplitUtil.ParseColor(TextSplitUtil.GetParseString(text, "color"));
+            boolean center = customText.center;
+
+            ArrayList<String> subString = TextSplitUtil.TextSpit(text);  // 切割文本
+            ArrayList<String> parseSubText = new ArrayList<>();  // 解析后的文本
+
+
+            if (subString.isEmpty()) {
+                // 如果没有子文本，直接渲染文本
+                Color color = TextSplitUtil.ParseColor(TextSplitUtil.GetParseString(text, "color"));  // 解析颜色
                 g2d.setFont(font);
                 g2d.setColor(color);
                 g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                //TextSplitUtil.GetParseString(text, "string") 文字提取
-                //渲染字符串
+                // 判断center属性，调整绘制位置
+                if (center) {
+                    // 计算文本宽度
+                    int textWidth = TextSplitUtil.getStringWidth(font, TextSplitUtil.GetParseString(text, "string"));
+                    // 计算居中位置
+                    x -= textWidth / 2;
+                }
                 g2d.drawString(TextSplitUtil.GetParseString(text, "string"), x, z);
             } else {
-                int TotalSubTextWidth = 0;
-                //字符串拼接 由子字符串的数量决定拼接几次
-                for (int i = 0; i < SubString.size(); i++) {
-                    //获取字符串内容
-                    String SubText = SubString.get(i);
-                    Color color = TextSplitUtil.ParseColor(TextSplitUtil.GetParseString(SubText, "color"));
-//                    for (int j = ParseSubText.size() - 1 ; j >= 0 ; j-- ){
-//                        TotalSubTextWidth = TotalSubTextWidth + TextSplitUtil.getStringWidth(font,ParseSubText.get(j));
-//                    }
-                    if (i - 1 >= 0)
-                        TotalSubTextWidth += TextSplitUtil.getStringWidth(font, ParseSubText.get(i - 1));
-                    ParseSubText.add(TextSplitUtil.GetParseString(SubText, "string"));
-//                    x += TotalSubTextWidth;
+                int totalSubTextWidth = 0;
+                for (int i = 0; i < subString.size(); i++) {
+                    String subText = subString.get(i);
+                    Color color = TextSplitUtil.ParseColor(TextSplitUtil.GetParseString(subText, "color"));
+                    if (i - 1 >= 0) {
+                        totalSubTextWidth += TextSplitUtil.getStringWidth(font, parseSubText.get(i - 1));
+                    }
+                    parseSubText.add(TextSplitUtil.GetParseString(subText, "string"));
                     g2d.setFont(font);
                     g2d.setColor(color);
                     g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                    g2d.drawString(ParseSubText.get(i), x + TotalSubTextWidth, z);
+                    // 判断center属性，调整绘制位置
+                    if (center) {
+                        // 计算文本宽度
+                        int textWidth = TextSplitUtil.getStringWidth(font, parseSubText.get(i));
+                        // 计算居中位置
+                        x -= textWidth / 2;
+                    }
+                    g2d.drawString(parseSubText.get(i), x + totalSubTextWidth, z);
                 }
             }
         }
         return srcImg;
     }
 }
-
